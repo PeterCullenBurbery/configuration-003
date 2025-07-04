5962d3d96deab2b9f6c0d9d43e195a8d56812a67
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-07-04 15:44:37 -0400

    Update main.go

--- go_projects/install_nirsoft/main.go
+++ go_projects/install_nirsoft/main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	// Constants
-	downloadURL := "https://github.com/PeterCullenBurbery/configuration-003/raw/main/nirsoft_package_enc_1.30.19.zip"
+	downloadURL := "https://raw.githubusercontent.com/PeterCullenBurbery/Nirsoft/main/nirsoft_package_enc_1.30.19.zip"
 	baseDir := `C:\downloads\nirsoft`
 	password := "nirsoft9876$"
 
