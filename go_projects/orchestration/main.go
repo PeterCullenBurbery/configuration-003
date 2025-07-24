8fa3fc25072689815c9897eb4f405b2575248f0b
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-07-24 13:59:00 -0400

    Add clean_path.exe execution to orchestration
    The orchestration script now runs clean_path.exe at the start of the process. This ensures the environment is cleaned before proceeding with further setup steps.

--- go_projects/orchestration/main.go
+++ go_projects/orchestration/main.go
@@ -30,11 +30,13 @@ func main() {
 
 	base_dir := os.Args[1]
 
+	clean_path_exe := filepath.Join(base_dir, "go_projects", "clean_path", "clean_path.exe")
 	install_things_path := filepath.Join(base_dir, "go_projects", "install", "install_things", "install_things.exe")
 	configuration_path := filepath.Join(base_dir, "go_projects", "configuration", "configuration", "configuration.exe")
 	configure_powershell_path := filepath.Join(base_dir, "go_projects", "powershell", "configure_powershell_modules", "configure_powershell_modules.exe")
 	// install_pip_packages_path := filepath.Join(base_dir, "go_projects", "install_pip_packages", "install_pip_packages.exe")
 
+	run_executable("clean_path.exe", clean_path_exe)
 	run_executable("install_things.exe", install_things_path, base_dir)
 	run_executable("configuration.exe", configuration_path, base_dir)
 	run_executable("configure_powershell_modules.exe", configure_powershell_path, base_dir)
