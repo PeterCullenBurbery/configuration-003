13dfc8b2f85bc11ed92f7b7411d6fffc02ffd5ca
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-11 19:59:00 -0400

    Comment out Python installer in main.go
    The 'install_Python' step has been commented out in the installation sequence, likely to temporarily disable Python installation. The executable was also rebuilt to reflect this change.

--- go_projects/install/install_things/main.go
+++ go_projects/install/install_things/main.go
@@ -29,7 +29,7 @@ func main() {
 		{"📦 install_packages", "install_packages.exe", []string{yamlPath}},
 		{"🍒 install_cherry_tree", "install_cherry_tree.exe", nil},
 		// {"🐍 install_miniconda", "install_miniconda.exe", nil},
-		{"🐍 install_Python", "install_Python.exe", nil},
+		// {"🐍 install_Python", "install_Python.exe", nil},
 		{"🧠 install_sql_developer", "install_sql_developer.exe", nil},
 		{"🧰 install_nirsoft", "install_nirsoft.exe", nil},
 		{"🔧 install_sys_internals", "install_sys_internals.exe", nil},
